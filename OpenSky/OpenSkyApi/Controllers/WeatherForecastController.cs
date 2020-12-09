e71182fada5fa5cbdab9b87ca61d8e0eb13cba35
Shaked Sibony <Shaked2009@gmail.com>
2020-12-10 00:22:55 +0200

    Add OpenSky to the Prog

---
+++ OpenSky/OpenSkyApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace OpenSkyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> logger;
+        private readonly FlightDbContext dbContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, FlightDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Flight>> Get()
+        {
+            HttpClient client = new HttpClient();
+            var responseStream = await client.GetStringAsync("https://opensky-network.org/api/states/all");
+            var rawFlights = JsonSerializer.Deserialize<RawFlights>(responseStream);
+
+            var currentFlights = rawFlights.states
+                .Where(data => (data?.Length ?? 0) >= 12)
+                .Select(raw => Flight.FromRawData(raw))
+                .ToDictionary(f => f.Icao24);
+
+            var dbFlights = await dbContext.Flights.ToListAsync();
+            var dbFlightsIds = dbFlights.Select(f => f.Icao24).ToHashSet();
+
+            var dbExistingFlights = dbFlights.Where(f => currentFlights.ContainsKey(f.Icao24) && f.LastContact < currentFlights[f.Icao24].LastContact);
+            foreach (var flight in dbExistingFlights)
+            {
+                var currFlight = currentFlights[flight.Icao24];
+                flight.BaroAltitude = currFlight.BaroAltitude;
+                flight.CallSign = currFlight.CallSign;
+                flight.LastContact = currFlight.LastContact;
+                flight.Latitude = currFlight.Latitude;
+                flight.Longitude = currFlight.Longitude;
+                flight.OnGround = currFlight.OnGround;
+                flight.OriginCountry = currFlight.OriginCountry;
+                flight.TimePosition = currFlight.TimePosition;
+                flight.TrueTrack = currFlight.TrueTrack;
+                flight.Velocity = currFlight.Velocity;
+                flight.VerticalRate = currFlight.VerticalRate;
+            }
+
+            var dbNewFlights = currentFlights.Where(f => !dbFlightsIds.Contains(f.Key));
+            dbContext.Flights.AddRange(dbNewFlights.Select(f => f.Value));
+
+            await dbContext.SaveChangesAsync();
+
+            return dbContext.Flights.Take(10);
+        }
+    }
+}
---
+++ OpenSky/OpenSkyApi/Flight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenSkyApi
+{
+    public class Flight
+    {
+        [Key]
+        public string Icao24 { get; set; }
+        public string CallSign { get; set; }
+        public string OriginCountry { get; set; }
+        public int? TimePosition { get; set; }
+        public int LastContact { get; set; }
+        public float? Longitude { get; set; }
+        public float? Latitude { get; set; }
+        public float? BaroAltitude { get; set; }
+        public bool OnGround { get; set; }
+        public float? Velocity { get; set; }
+        public float? TrueTrack { get; set; }
+        public float? VerticalRate { get; set; }
+
+        public static Flight FromRawData(object[] data)
+        {
+            return new Flight
+            {
+                Icao24 = data[0]?.ToString(),
+                CallSign = data[1]?.ToString(),
+                OriginCountry = data[2]?.ToString(),
+                TimePosition = data[3] != null ? int.Parse(data[3].ToString()) : null,
+                LastContact = int.Parse(data[4].ToString()),
+                Longitude = data[5] != null ? float.Parse(data[5].ToString()) : null,
+                Latitude = data[6] != null ? float.Parse(data[6].ToString()) : null,
+                BaroAltitude = data[7] != null ? float.Parse(data[7].ToString()) : null,
+                OnGround = bool.Parse(data[8].ToString()),
+                Velocity = data[9] != null ? float.Parse(data[9].ToString()) : null,
+                TrueTrack = data[10] != null ? float.Parse(data[10].ToString()) : null,
+                VerticalRate = data[11] != null ? float.Parse(data[11].ToString()) : null,
+            };
+        }
+    }
+
+    public class RawFlights
+    {
+        public int time { get; set; }
+        public object[][] states { get; set; }
+    }
+}
