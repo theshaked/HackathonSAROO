55dde7f7306eb561dd196f77c2d79f3257b0c004
OrelZluf <orelz16@gmail.com>
2020-12-10 08:33:21 +0200

    split the components to different components files. added button on the map.

---
+++ src/components/buttonContainer/buttonContainer.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import {Buttons} from "../button/button";
+import styles from './buttonContainer.module.css';
+//
+// // 'coffee', 'adjust',
+// var leftButtons = ['search-plus'];
+// var rightButtons = ['search-minus'];
+//
+// export class ButtonContainer extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <div className={styles.buttonContainerLeft}>
+//                 {leftButtons.map((buttonClass) => {
+//                     return <Buttons side="left" class={buttonClass}/>
+//                 })}
+//                 </div>
+//                 <div className={styles.buttonContainerRight}>
+//                     {rightButtons.map((buttonClass) => {
+//                         return <Buttons side="right" class={buttonClass}/>
+//                     })}
+//                 </div>
+//             </div>
+//
+//         );
+//     }
+// }
